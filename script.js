1197184edaba6e07f2b4e7b3de0cc7f7608f271b
ItzValen01 <valentinomodaffari07@gmail.com>
2026-04-15 21:30:24 -0300

    feat: ejercicio 4 - mas marcadores

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var marker = L.marker([-34.6037, -58.3816]).addTo(map)
 marker.bindPopup("Hola! Soy un popup 😄")
+
+var marker1 = L.marker([-37.321823869219806, -59.13110738934614]).addTo(map)
+marker1.bindPopup("Plaza de las carretas")
+
+var marker2 = L.marker([-37.328624144157516, -59.136935173456976]).addTo(map)
+marker2.bindPopup("Plaza Independencia")
+
+var marker3 = L.marker([-37.32761825314754, -59.15236740327897]).addTo(map)
+marker3.bindPopup("Monte Calvario")
