d4b3fa7a70d2548fdf3fc201706c911fd98876fd
ItzValen01 <valentinomodaffari07@gmail.com>
2026-04-15 21:19:11 -0300

    feat: ejercicio 2 - marcador

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ var map = L.map('map').setView([-34.6037, -58.3816], 13)
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
-  }).addTo(map);
+}).addTo(map);
+
+var marker = L.marker([-34.6037, -58.3816]).addTo(map)
